fabf5784fdbe06ba9cafc32ceb4a5a90bf756512
mac <mac@macs-iMac.local>
2021-05-25 12:29:21 +0800

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Azali
+                     MyLaravel V2.0
                 </div>
 
                 <div class="links">
