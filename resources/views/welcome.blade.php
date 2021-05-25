d40c2d3b47e1c3494db2c83473e4e6e26d8a04ad
azalinor <80928727+azalinor@users.noreply.github.com>
2021-05-25 11:38:18 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Nino
                 </div>
 
                 <div class="links">
